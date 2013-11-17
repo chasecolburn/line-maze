b22d49eb1c05a56bdb909023dc90debbb3942928
Chase Colburn <chase@cerego.com>
2013-11-18 00:49:16 +0900

    Migration to SDK 2.0

--- src/optical_watch.c
+++ src/optical_watch.c
@@ -1,17 +1,13 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
-// Define application
-#define AUTO_GENERATED_UUID { 0xAF, 0x77, 0xC6, 0x54, 0x47, 0x51, 0x42, 0x10, 0xB1, 0x92, 0xB1, 0x69, 0x5A, 0x18, 0xD8, 0x63 }
-PBL_APP_INFO(AUTO_GENERATED_UUID, "Line Maze", "Chase Colburn", 1, 1, DEFAULT_MENU_ICON, APP_INFO_WATCH_FACE);
+Window *window;
+Layer *leftHourLayer;
+Layer *rightHourLayer;
+Layer *leftMinuteLayer;
+Layer *rightMinuteLayer;
 
-Window window;
-
-Layer leftHourLayer;
-Layer rightHourLayer;
-Layer leftMinuteLayer;
-Layer rightMinuteLayer;
+GBitmap *patternOnImage;
+GBitmap *patternOffImage;
 
 int hours = 0, minutes = 0;
 // Use the low order first 20 bits to define each number
@@ -26,14 +22,12 @@ int numbers[10] = {0xF999F, /* 0 */
                    0xF9F9F, /* 8 */
                    0x88F9F  /* 9 */};
 
-BmpContainer patternOnContainer;
-BmpContainer patternOffContainer;
-
 //-----------------------------------------------------------------------------
 // Interface
 
-void handle_init(AppContextRef ctx);
-void handle_tick(AppContextRef ctxt, PebbleTickEvent *event);
+void handle_init();
+void handle_deinit();
+void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed);
 void draw_layer(Layer *layer, GContext *gctxt);
 void draw_time_layer(Layer *layer, GContext *gctxt);
 int display_number_for_layer(Layer *layer);
@@ -41,84 +35,95 @@ int display_number_for_layer(Layer *layer);
 //-----------------------------------------------------------------------------
 // Implementation
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-    .tick_info =
-    {
-      .tick_handler = &handle_tick,
-      .tick_units = MINUTE_UNIT
-    }
-  };
-
-  app_event_loop(params, &handlers);
+int main(void) {
+  handle_init();
+  app_event_loop();
+  handle_deinit();
 }
 
-void handle_init(AppContextRef ctx) {
-  window_init(&window, "Line Maze");
-  window_stack_push(&window, true /* Animated */);
-    
-  resource_init_current_app(&APP_RESOURCES);
+void handle_init() {
+  patternOnImage = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PATTERN_ON);
+  patternOffImage = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PATTERN_OFF);
   
-  bmp_init_container(RESOURCE_ID_IMAGE_PATTERN_ON, &patternOnContainer);
-  bmp_init_container(RESOURCE_ID_IMAGE_PATTERN_OFF, &patternOffContainer);
+  window = window_create();
+  window_stack_push(window, true /* Animated */);
+    
+  time_t now = time(NULL);
+  struct tm *tick_time = localtime(&now);
+  hours = tick_time->tm_hour;
+  minutes = tick_time->tm_min;
 
-  Layer *root = window_get_root_layer(&window);
+  Layer *root = window_get_root_layer(window);
   layer_set_update_proc(root, draw_layer);
   layer_mark_dirty(root);
 
-  layer_init(&leftHourLayer, GRect(12, 12, 48, 60));
-  layer_set_update_proc(&leftHourLayer, draw_time_layer);
-  layer_add_child(root, &leftHourLayer);
+  leftHourLayer = layer_create(GRect(12, 12, 48, 60));
+  layer_set_update_proc(leftHourLayer, draw_time_layer);
+  layer_add_child(root, leftHourLayer);
+  layer_mark_dirty(leftHourLayer);
 
-  layer_init(&rightHourLayer, GRect(84, 12, 48, 60));
-  layer_set_update_proc(&rightHourLayer, draw_time_layer);
-  layer_add_child(root, &rightHourLayer);
+  rightHourLayer = layer_create(GRect(84, 12, 48, 60));
+  layer_set_update_proc(rightHourLayer, draw_time_layer);
+  layer_add_child(root, rightHourLayer);
+  layer_mark_dirty(rightHourLayer);
 
-  layer_init(&leftMinuteLayer, GRect(12, 96, 48, 60));
-  layer_set_update_proc(&leftMinuteLayer, draw_time_layer);
-  layer_add_child(root, &leftMinuteLayer);
+  leftMinuteLayer = layer_create(GRect(12, 96, 48, 60));
+  layer_set_update_proc(leftMinuteLayer, draw_time_layer);
+  layer_add_child(root, leftMinuteLayer);
+  layer_mark_dirty(leftMinuteLayer);
 
-  layer_init(&rightMinuteLayer, GRect(84, 96, 48, 60));
-  layer_set_update_proc(&rightMinuteLayer, draw_time_layer);
-  layer_add_child(root, &rightMinuteLayer);
+  rightMinuteLayer = layer_create(GRect(84, 96, 48, 60));
+  layer_set_update_proc(rightMinuteLayer, draw_time_layer);
+  layer_add_child(root, rightMinuteLayer);
+  layer_mark_dirty(rightMinuteLayer);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+}
+
+void handle_deinit() {
+  tick_timer_service_unsubscribe();
+  gbitmap_destroy(patternOnImage);
+  gbitmap_destroy(patternOffImage);
+  layer_destroy(leftHourLayer);
+  layer_destroy(rightHourLayer);
+  layer_destroy(leftMinuteLayer);
+  layer_destroy(rightMinuteLayer);
+  window_destroy(window);
 }
 
 // Any update to the screen causes the entire row to update so might as well mark both numbers dirty at the same time
-void handle_tick(AppContextRef ctxt, PebbleTickEvent *event)   {
-  PblTm time;
-  get_time(&time);
-
-  if(time.tm_hour != hours) {
-    hours = time.tm_hour;
-    layer_mark_dirty(&leftHourLayer);
-    layer_mark_dirty(&rightHourLayer);
+void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed)   {
+  if(units_changed & HOUR_UNIT) {
+    hours = tick_time->tm_hour;
+    layer_mark_dirty(leftHourLayer);
+    layer_mark_dirty(rightHourLayer);
   }
 
-  // Since this event is triggered every minute, these will always need to be redrawn
-  minutes = time.tm_min;
-  layer_mark_dirty(&leftMinuteLayer);
-  layer_mark_dirty(&rightMinuteLayer);
+  if(units_changed & MINUTE_UNIT) {
+    minutes = tick_time->tm_min;
+    layer_mark_dirty(leftMinuteLayer);
+    layer_mark_dirty(rightMinuteLayer);
+  }
 }
 
 // Draw entire screen
 void draw_layer(Layer *layer, GContext *gctxt) {
-  graphics_draw_bitmap_in_rect(gctxt, &patternOffContainer.bmp, GRect(0, 0, 144, 168));
+  graphics_draw_bitmap_in_rect(gctxt, patternOffImage, GRect(0, 0, 144, 168));
 }
 
 // Draw layer for each number
 void draw_time_layer(Layer *layer, GContext *gctxt) {
   GRect destination = GRect(0, 0, 48, 60);
-  graphics_draw_bitmap_in_rect(gctxt, &patternOffContainer.bmp, destination);
+  graphics_draw_bitmap_in_rect(gctxt, patternOffImage, destination);
 
   int displayNumber = display_number_for_layer(layer);
   for(int row = 0; row < 5; row++) {
     for(int col = 0; col < 4; col++) {
       destination = GRect(col * 12, row * 12, 12, 12);
       if(numbers[displayNumber] & (1 << ((row * 4) + col))) {
-        graphics_draw_bitmap_in_rect(gctxt, &patternOnContainer.bmp, destination);  
+        graphics_draw_bitmap_in_rect(gctxt, patternOnImage, destination);  
       }else {
-        graphics_draw_bitmap_in_rect(gctxt, &patternOffContainer.bmp, destination);
+        graphics_draw_bitmap_in_rect(gctxt, patternOffImage, destination);
       }
     }
   }
@@ -127,13 +132,13 @@ void draw_time_layer(Layer *layer, GContext *gctxt) {
 // Return number to display for the target layer
 int display_number_for_layer(Layer *layer) {
   // Hour 10s place
-  if(layer == &leftHourLayer) return hours / 10;
-  // Hour 1s place
-  if(layer == &rightHourLayer) return hours % 10;
+  if(layer == leftHourLayer) return hours / 10;
+  // Hour 1s pace
+  if(layer == rightHourLayer) return hours % 10;
   // Minute 10s place
-  if(layer == &leftMinuteLayer) return minutes / 10;
+  if(layer == leftMinuteLayer) return minutes / 10;
   // Minute 1s place
-  if(layer == &rightMinuteLayer) return minutes % 10;
+  if(layer == rightMinuteLayer) return minutes % 10;
   // Default (should never reach here)
   return 0;
 }
