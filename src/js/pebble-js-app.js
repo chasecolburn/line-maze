1e55367d0a2613dfc54ccd19ce56fab381651929
Daniel Bornkessel <daniel@soundcloud.com>
2014-03-14 11:04:24 +0100

    Cleanup js, introduce logging flag

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,56 +1,65 @@
 var initialized = false;
-var shakeToCheat = localStorage.getItem("shake-to-cheat") || 1;
+var log = false; // set to true to get some debugging logging
+
+function log(text) {
+  if(log) {
+    console.log(text)
+  }
+}
 
 Pebble.addEventListener("ready", function() {
-  console.log("ready called!");
+  log("ready called!");
   initialized = true;
   Pebble.sendAppMessage( { "js-initialized": 1 },
                         function(e) {
-                          console.log("Successfully told pebble that I am ready");
+                          log("Successfully told pebble that I am ready");
                         },
                         function(e) {
-                          console.log("Failed telling pebble that I am ready: "+ e.error.message);
+                          log("Failed telling pebble that I am ready: "+ e.error.message);
                         }
                        );
 });
 
 Pebble.addEventListener("showConfiguration", function() {
-  console.log("showing configuration");
+  log("showing configuration");
+  var shakeToCheat = localStorage.getItem("shake-to-cheat") || 1;
   Pebble.openURL('http://rawgithub.com/kesselborn/line-maze/SDK-2.0-with-shake-to-cheat-config/configuration.html?stc=' + shakeToCheat);
 });
 
-  Pebble.addEventListener("appmessage", function(e) {
-    var command = e.payload["command"];
+function setShakeToCheat(shakeToCheat) {
+Pebble.sendAppMessage( { "shake-to-cheat": shakeToCheat},
+                      function(e) {
+                        log("Successfully delivered message with transactionId=" +
+                            e.data.transactionId + " ... shake-to-cheat=" + options["shake-to-cheat"]);
+                      },
+                      function(e) {
+                        log("Unable to deliver message with transactionId=" +
+                            e.data.transactionId +
+                              " Error is: " + e.error.message);
+                      }
+                     );
 
-    if(command === "send-config") {
-      console.log("sending config to pebble as requested");
-      setShakeToCheat(shakeToCheat);
-    }
-  });
+}
 
-  function setShakeToCheat(shakeToCheat) {
-    Pebble.sendAppMessage( { "shake-to-cheat": shakeToCheat},
-                          function(e) {
-                            console.log("Successfully delivered message with transactionId=" +
-                                        e.data.transactionId + " ... shake-to-cheat=" + options["shake-to-cheat"]);
-                          },
-                          function(e) {
-                            console.log("Unable to deliver message with transactionId=" +
-                                        e.data.transactionId +
-                                          " Error is: " + e.error.message);
-                          }
-                         );
+Pebble.addEventListener("appmessage", function(e) {
+  var command = e.payload["command"];
+  var shakeToCheat = localStorage.getItem("shake-to-cheat") || 1;
 
+  if(command === "send-config") {
+    log("sending config to pebble as requested");
+    setShakeToCheat(shakeToCheat);
   }
+});
 
-  Pebble.addEventListener("webviewclosed", function(e) {
-    console.log("configuration closed");
-    // webview closed
-    var options = JSON.parse(decodeURIComponent(e.response));
 
-    console.log("sending config to pebble");
-    localStorage.setItem("shake-to-cheat", options["shake-to-cheat"]);
-    setShakeToCheat(options["shake-to-cheat"]);
+Pebble.addEventListener("webviewclosed", function(e) {
+  log("configuration closed");
+  // webview closed
+  var options = JSON.parse(decodeURIComponent(e.response));
 
-    console.log("Options = " + JSON.stringify(options));
-  });
+  log("sending config to pebble");
+  localStorage.setItem("shake-to-cheat", options["shake-to-cheat"]);
+  setShakeToCheat(options["shake-to-cheat"]);
+
+  log("Options = " + JSON.stringify(options));
+});
